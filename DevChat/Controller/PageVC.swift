fc2d5c2823a0975056faf2bb426fca1ab2f37bdf
rkung <rex.h.kung@gmail.com>
2018-03-08 22:39:07 -0700

    Adding in pagecontrol

---
+++ DevChat/Controller/PageVC.swift
@@ -0,0 +1,21 @@
+//
+//  PageVC.swift
+//  DevChat
+//
+//  Created by Rex Kung on 3/8/18.
+//  Copyright © 2018 Rex Kung. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
