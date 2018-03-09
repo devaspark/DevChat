b44ad5be508cb7a1e90dca0b090b8f7901ac917e
rkung <rex.h.kung@gmail.com>
2018-03-08 23:24:39 -0700

    Pagecontrol Working

--- DevChat/Controller/PageVC.swift
+++ DevChat/Controller/PageVC.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class PageVC: UIPageViewController {
+class PageVC: UIPageViewController, UIPageViewControllerDataSource {
+    
+    lazy var viewControllerList:[UIViewController] = {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "VideoView")
+        let vc2 = sb.instantiateViewController(withIdentifier: "CameraView")
+        
+        
+        return [vc1, vc2]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.dataSource = self
         // Do any additional setup after loading the view.
+        
+        if let firstViewController = viewControllerList.last {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
     }
 
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else { return nil }
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else { return nil }
+        
+        guard viewControllerList.count > previousIndex else { return nil }
+        
+        return viewControllerList[previousIndex]
+        
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
 
+        guard let vcIndex = viewControllerList.index(of: viewController) else { return nil }
+
+        let nextIndex = vcIndex + 1
+        
+        guard nextIndex <= viewControllerList.count else { return nil }
+        
+        guard viewControllerList.count > nextIndex else { return nil }
+        
+        return viewControllerList[nextIndex]
+    }
 
 }
